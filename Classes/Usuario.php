86337545f358bc46c8961453d9739d1722de27ac
Moises Pontes <sesiom_assis@hotmail.com>
2019-10-27 19:17:57 -0300

    Atualização das classes

--- Classes/Usuario.php
+++ Classes/Usuario.php
@@ -1,16 +1,17 @@
 <?php
+
 /**
  * Description of Usuario
  *
  * @author Moises Pontes
  */
 class Usuario{
-    
+
     private $id_usuario;
     private $deslogin;
     private $dessenha;
     private $dtcadastro;
-    
+
     function getId_usuario(){
         return $this->id_usuario;
     }
@@ -26,6 +27,7 @@ class Usuario{
     function getDtcadastro(){
         return $this->dtcadastro;
     }
+
     function setId_usuario($id_usuario){
         $this->id_usuario = $id_usuario;
     }
@@ -41,12 +43,12 @@ class Usuario{
     function setDtcadastro($dtcadastro){
         $this->dtcadastro = $dtcadastro;
     }
-    
+
     public function loadById($id){
-        
+
         $sql = new Sql();
-        $results = $sql->select("SELECT * FROM tb_usuarios WHERE id_usuario = :id", array(':id'=>$id));
-        if(count($results)>0){
+        $results = $sql->select("SELECT * FROM tb_usuarios WHERE id_usuario = :id", array(':id' => $id));
+        if(count($results) > 0){
             $row = $results[0];
             $this->setId_usuario($row['id_usuario']);
             $this->setDeslogin($row['deslogin']);
@@ -54,16 +56,39 @@ class Usuario{
             $this->setDtcadastro(new \DateTime($row['dtcadastro']));
         }
     }
-    
+
+    public function getList($Quary){
+        $sql = new Sql();
+        return $sql->select($Quary);
+    }
+
+    public static function search($Login){
+        $sql = new Sql();
+        return $sql->select("SELECT * FROM tb_usuarios LIKE :SEARCH ORDER BY deslogin", array(':SEARCH' => '%' . $Login . '%'));
+    }
+
+    public function login($Login,$senha){
+        $sql = new Sql();
+        $results = $sql->select("SELECT * FROM tb_usuarios WHERE deslogin = :login AND dessenha = :password", array(":login" => $Login, ":password" => $senha));
+        if(count($results) > 0){
+            $row = $results[0];
+            $this->setId_usuario($row['id_usuario']);
+            $this->setDeslogin($row['deslogin']);
+            $this->setDessenha($row['dessenha']);
+            $this->setDtcadastro(new \DateTime($row['dtcadastro']));
+        }
+        else{
+            throw new Exception('Login e/ou Senha Inválidos');
+        }
+    }
+
     public function __toString(){
         return json_encode(array(
             'Id User' => $this->getId_usuario(),
             'Login' => $this->getDeslogin(),
             'Senha' => $this->getDessenha(),
             'Cadastro' => $this->getDtcadastro()->format('d/m/Y')
-        
         ));
     }
-
 
 }
--- index.php
+++ index.php
@@ -5,6 +5,22 @@ require_once ('config.php');
 //$sql = new Sql();
 //$usuarios = $sql->select('SELECT * FROM tb_usuarios');
 //echo json_encode($usuarios);
-$select = new Usuario();
-$select->loadById('14');
-echo $select;
+//$select = new Usuario();
+//$select->loadById('14');
+//echo $select;
+//$sql =  new Usuario();
+//$list = $sql->getList("SELECT * FROM tb_usuarios ORDER BY deslogin;");
+//
+//var_dump($list[0]);
+//foreach($list as $value){
+//    extract($value);
+//    
+//    echo 'Id: '.$id_usuario .'<br>';
+//    echo 'Login: '.$deslogin .'<br>';
+//    echo 'Senha: '.$dessenha .'<br>';
+//    echo 'Data Cad: '.$dtcadastro.'<br>';
+//}
+
+$login =  new Usuario();
+$login->login("Moise", "001001");
+echo $login;
